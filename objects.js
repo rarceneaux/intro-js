c23bfba83e5e8cd6d03a9c10d7cbe6e68a914bb7
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-08-24 12:31:12 -0500

    added something

---
+++ objects.js
@@ -0,0 +1,12 @@
+const bouncer = (age) =>{
+  if (age >= 21) {
+    console.log('DRINK (at the bar')
+  } else {
+    console.log('Shame');
+  }
+  };
+bouncer(12);
+bouncer(93);
+
+const me = 'martin';
+console.log(me.length);
